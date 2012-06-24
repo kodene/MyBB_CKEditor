f5b67ef79b83f5d2320ee6cb56d1e16c08367711
Brian McCloskey <koden92@gmail.com>
2012-06-24 01:35:00 -0600

    Updated mybb_config
    Removed smiley_path config as it's now passed to the CKEditor instance
    from the MyBB plugin.
    Added Indent and Outdent buttons.
    Added keystrokes config to include default settings plus two additional
    for Indent and Outdent.

--- ckeditor/mybb_config.js
+++ ckeditor/mybb_config.js
@@ -1,8 +1,7 @@
 ﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
+Copyright (c) 2012, Brian McCloskey. All rights reserved.
+For licensing, see LICENSE, or <http://www.gnu.org/licenses/>
 */
-
 CKEDITOR.editorConfig = function( config )
 {
 	config.extraPlugins = 'mybb,autogrow,mybbcode,mybbvideo';
@@ -10,20 +9,42 @@ CKEDITOR.editorConfig = function( config )
 	config.fontSize_sizes = 'XX Small/xx-small;X Small/x-small;Small/small;Medium/medium;Large/large;X Large/x-large;XX Large/xx-large';
 	config.font_names = 'Arial;Courier;Impact;Tahoma;Times New Roman;Trebuchet MS;Verdana';
 	config.skin = 'mybb';
-	config.smiley_path = '/';
 	config.forcePasteAsPlainText = true;
 	config.autoGrow_maxHeight = '500';
 	config.dialog_backgroundCoverColor = 'rgb(0,0,0)';
 	config.dialog_backgroundCoverOpacity = 0.5;
 	config.enterMode = CKEDITOR.ENTER_BR;
+
+	config.keystrokes =
+	[
+		[ CKEDITOR.ALT + 121 /*F10*/, 'toolbarFocus' ],
+		[ CKEDITOR.ALT + 122 /*F11*/, 'elementsPathFocus' ],
 	
+		[ CKEDITOR.SHIFT + 121 /*F10*/, 'contextMenu' ],
+	
+		[ CKEDITOR.CTRL + 90 /*Z*/, 'undo' ],
+		[ CKEDITOR.CTRL + 89 /*Y*/, 'redo' ],
+		[ CKEDITOR.CTRL + CKEDITOR.SHIFT + 90 /*Z*/, 'redo' ],
+	
+		[ CKEDITOR.CTRL + 76 /*L*/, 'link' ],
+	
+		[ CKEDITOR.CTRL + 66 /*B*/, 'bold' ],
+		[ CKEDITOR.CTRL + 73 /*I*/, 'italic' ],
+		[ CKEDITOR.CTRL + 85 /*U*/, 'underline' ],
+	
+		[ CKEDITOR.ALT + 109 /*-*/, 'toolbarCollapse' ],
+
+		[ CKEDITOR.CTRL + CKEDITOR.ALT + 73 /*I*/, 'indent' ],
+		[ CKEDITOR.CTRL + CKEDITOR.ALT + 79 /*O*/, 'outdent' ]
+	];
+
 	config.toolbar =
 	[
 		['Source','-','Maximize','-','Undo','Redo','Save'],
-		['Font','FontSize','TextColor'], 
+		['Font','FontSize','TextColor'],
 		['Bold','Italic','Underline','Strike','-','JustifyLeft','JustifyCenter','JustifyRight','-','RemoveFormat'],
 		'/',
-		['NumberedList','BulletedList','-','HorizontalRule'],
+		['NumberedList','BulletedList','Indent','Outdent','-','HorizontalRule'],
 		['Link','Unlink','-','Blockquote','Code','PHP','-','Image','Video'],
 		['Smiley','SpecialChar'],
 	];
