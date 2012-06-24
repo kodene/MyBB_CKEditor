48d376d85602901bff0b07b0d64a3c9d2fdf0261
Brian McCloskey <koden92@gmail.com>
2012-06-24 01:37:23 -0600

    Updated headers.

--- ckeditor/_source/plugins/mybbvideo/plugin.js
+++ ckeditor/_source/plugins/mybbvideo/plugin.js
@@ -1,21 +1,6 @@
 /*
 Copyright (c) 2012, Brian McCloskey. All rights reserved.
-For licensing, see LICENSE.
-
-This file is part of MYBB_CKEditor.
-
-MYBB_CKEditor is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-MYBB_CKEditor is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with MYBB_CKEditor.  If not, see <http://www.gnu.org/licenses/>.
+For licensing, see LICENSE, or <http://www.gnu.org/licenses/>
 */
 (function()
 {
