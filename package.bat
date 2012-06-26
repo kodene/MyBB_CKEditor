dfe1f51c67e9dbacc613a307221b0b986b6179b7
Brian McCloskey <koden92@gmail.com>
2012-06-26 08:45:51 -0600

    Updated package script
    More specific build to trim down install of files that aren't needed.

--- package.bat
+++ package.bat
@@ -17,25 +17,26 @@ rmdir /S /Q Upload
 
 :: Create inital directories
 mkdir %MYBB%
-mkdir %CKEDITOR%
+mkdir %CKEDITOR%\plugins
 
 :: Copy MyBB content into Upload
 xcopy /Q /E /Y mybb\* %MYBB%
 
 :: Copy CKEditor content into Upload
-xcopy /Q /E /Y %CKBUILD%\images			%CKEDITOR%\images\
-xcopy /Q /E /Y %CKBUILD%\lang			%CKEDITOR%\lang\
-xcopy /Q /E /Y %CKBUILD%\plugins		%CKEDITOR%\plugins\
-xcopy /Q /E /Y %CKBUILD%\skins			%CKEDITOR%\skins\
-xcopy /Q /E /Y %CKBUILD%\themes			%CKEDITOR%\themes\
-copy %CKBUILD%\ckeditor.js				%CKEDITOR%\
-copy %CKBUILD%\mybb_config.js			%CKEDITOR%\
+xcopy /Q /E /Y %CKBUILD%\images				%CKEDITOR%\images\
+xcopy /Q /E /Y %CKBUILD%\plugins\image		%CKEDITOR%\plugins\image\
+xcopy /Q /E /Y %CKBUILD%\plugins\link		%CKEDITOR%\plugins\link\
+xcopy /Q /E /Y %CKBUILD%\plugins\mybb		%CKEDITOR%\plugins\mybb\
+xcopy /Q /E /Y %CKBUILD%\plugins\mybbcode	%CKEDITOR%\plugins\mybbcode\
+xcopy /Q /E /Y %CKBUILD%\plugins\mybbvideo	%CKEDITOR%\plugins\mybbvideo\
+xcopy /Q /E /Y %CKBUILD%\plugins\smiley		%CKEDITOR%\plugins\smiley\
+xcopy /Q /E /Y %CKBUILD%\skins\mybb			%CKEDITOR%\skins\mybb\
+xcopy /Q /E /Y %CKBUILD%\themes				%CKEDITOR%\themes\
+copy %CKBUILD%\ckeditor.js					%CKEDITOR%\
+copy %CKBUILD%\mybb_config.js				%CKEDITOR%\
+copy %CKBUILD%\lang\en.js					%CKEDITOR%\lang\
 
 :: Create archive
-cd Upload
-7z a -ttar MyBB_CKEditor.tar inc jscripts
-7z a -tgzip MyBB_CKEditor.tar.gz MyBB_CKEditor.tar
-move MyBB_CKEditor.tar ..
-move MyBB_CKEditor.tar.gz ..
+7z a -tzip MyBB_CKEditor-PACKAGE.zip Upload LICENSE README
 
 pause
